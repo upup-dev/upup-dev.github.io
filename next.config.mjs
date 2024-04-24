40f09330e59a0432d9871d335a17582f248decf2
Oleksandr Harahan <oharahan@iMac-Mak.local>
2024-04-25 01:52:35 +0300

    fix-res

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        domains: ['upup-dev.github.io'],
+    },
     output: "export",
     assetPrefix: process.env.NODE_ENV === 'production' ? '/upup.site-v2.0.github.io/' : '',
 };
