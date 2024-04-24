56973763a7eba33058d4260390506fadb5de0682
Oleksandr Harahan <oharahan@iMac-Mak.local>
2024-04-25 00:38:15 +0300

    fix

--- src/app/pages/services/websites/websites.tsx
+++ src/app/pages/services/websites/websites.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import styles from "./style.module.scss";
 import Header from "@/app/components/header/header";
 
-export default function Home() {
+export default function Websites() {
     return (
         <main className={styles.main}>
             <Header />
