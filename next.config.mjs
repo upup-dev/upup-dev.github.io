027a3c9d8817667f1147e496bcaf222498ae9433
Oleksandr Harahan <oharahan@iMac-Mak.local>
2024-04-25 01:26:06 +0300

    fix-env

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/upup.site-v2.0.github.io' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/upup.site-v2.0.github.io/' : '',
 };
 
 export default nextConfig;
