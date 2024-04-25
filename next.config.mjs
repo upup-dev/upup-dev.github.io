b5f0a96911ee104273d6e08479f2377937bb3158
Oleksandr Harahan <oharahan@iMac-Mak.local>
2024-04-25 12:35:31 +0300

    fix-path

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
+const path = require('path');
 
 const nextConfig = {
-    // output: "export",
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        // Вказуємо webpack, де розгортати аліаси
+        config.resolve.alias['@'] = path.join(__dirname);
+        return config;
+    },
     basePath: isProd ? '/upup.site-v2.0.github.io' : '',
     assetPrefix: isProd ? '/upup.site-v2.0.github.io' : '',
 };
