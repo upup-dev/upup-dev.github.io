58a6ec6355a7afd4d1463d1c9590090ad5bd4374
Oleksandr Harahan <oharahan@iMac-Mak.local>
2024-04-25 13:37:52 +0300

    fix-path

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-const isProd = process.env.NODE_ENV === 'production';
-
-const nextConfig = {
-    basePath: isProd ? '/upup.site-v2.0.github.io' : '',
-    assetPrefix: isProd ? '/upup.site-v2.0.github.io' : '',
-};
+const nextConfig = {};
 
 export default nextConfig;
-
