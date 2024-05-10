6bb76c4e5a143b0416f769ffa7d3d05979b4562a
Oleksandr Harahan <oharahan@iMac-Mak.local>
2024-05-10 10:41:38 +0300

    cooperation fix text

--- app/components/sections/teem/teem.tsx
+++ app/components/sections/teem/teem.tsx
@@ -6,7 +6,7 @@ const Teem = () => {
     return (
         <section className={`${styles.teem} teem`}>
             <div className={styles.teem_container}>
-                <p className={styles.teem_text}>Widely experienced tech professionals, motivated by our clients businesses' growth</p>
+                <p className={styles.teem_text}>{'Widely experienced tech professionals, motivated by our clients businesses\' growth'}</p>
                 <div className={styles.teem_wrapBtn}>
                     <Btn name={'Meet you digital team'} color={'#FFF'} white={true} fontSize={'font-32px'} url={'/'}/>
                 </div>
