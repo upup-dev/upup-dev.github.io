f164d3546a00c74a696f5d4db2ad526dd8f9dfe6
Oleksandr Harahan <oharahan@iMac-Mak.local>
2024-05-11 18:31:05 +0300

    cooperation model fix

--- app/components/sections/teem/teem.tsx
+++ app/components/sections/teem/teem.tsx
@@ -8,7 +8,7 @@ const Teem = () => {
             <div className={styles.teem_container}>
                 <p className={styles.teem_text}>{'Widely experienced tech professionals, motivated by our clients businesses\' growth'}</p>
                 <div className={styles.teem_wrapBtn}>
-                    <Btn name={'Meet you digital team'} color={'#FFF'} white={true} fontSize={'font-32px'} url={'/'}/>
+                    <Btn name={'Meet you digital team'} color={'#FFF'} white={true} fontSize={'font-18px'} url={'/'}/>
                 </div>
             </div>
         </section>
